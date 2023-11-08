ac8697e4c6e7ee0e712cfc298f758720c768ffc3
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-09 06:56:52 +0900

    SampleControllerを作成

--- src/main/java/com/example/SpringBootOpenApi/controller/HealthController.java
+++ src/main/java/com/example/SpringBootOpenApi/controller/HealthController.java
@@ -1,6 +1,13 @@
 package com.example.SpringBootOpenApi.controller;
 
 import com.example.todoapi.controller.HealthApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthController implements HealthApi {
+    @Override
+    public ResponseEntity<Void> healthGet() {
+        return ResponseEntity.ok().build();
+    }
 }
