e1fcf8e93ed9929be887bd769ebdf4ba62c28e03
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-09 07:24:21 +0900

    Lombokの導入

--- src/main/java/com/example/SpringBootOpenApi/controller/sample/SampleDTO.java
+++ src/main/java/com/example/SpringBootOpenApi/controller/sample/SampleDTO.java
@@ -1,30 +1,12 @@
 package com.example.SpringBootOpenApi.controller.sample;
 
+import lombok.Value;
+
 import java.time.LocalDateTime;
 
+@Value
 public class SampleDTO {
 
-    private String content;
-    private LocalDateTime timestamp;
-
-    public SampleDTO(String content, LocalDateTime timestamp) {
-        this.content = content;
-        this.timestamp = timestamp;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
+    String content;
+    LocalDateTime timestamp;
 }
