2d61e8c8bc3a4c47587c7e1e1c596d1e496a60bd
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-10 09:18:33 +0900

    tagを使ってスキーマを整理、Controllerの実装、メソッドのリファクタリング

---
+++ src/main/java/com/example/SpringBootOpenApi/controller/task/TaskController.java
@@ -0,0 +1,14 @@
+package com.example.SpringBootOpenApi.controller.task;
+
+import com.example.todoapi.controller.TasksApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController implements TasksApi {
+
+    @Override
+    public ResponseEntity<Void> showTask() {
+        return ResponseEntity.ok().build();
+    }
+}
