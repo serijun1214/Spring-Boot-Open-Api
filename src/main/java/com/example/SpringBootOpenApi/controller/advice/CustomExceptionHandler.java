3362de4b206c6b754438f362aa766803022a2427
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-10 14:53:37 +0900

    400 Bad Request（フィールド欠損）：例外ハンドラー実装

--- src/main/java/com/example/SpringBootOpenApi/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/SpringBootOpenApi/controller/advice/CustomExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.example.SpringBootOpenApi.controller.advice;
 
 import com.example.SpringBootOpenApi.service.task.TaskEntityNotFoundException;
+import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.ResourceNotFoundError;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(TaskEntityNotFoundException.class)
     public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
@@ -16,5 +22,15 @@ public class CustomExceptionHandler {
         error.setDetail(e.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        return ResponseEntity.badRequest().body(new BadRequestError());
     }
 }
