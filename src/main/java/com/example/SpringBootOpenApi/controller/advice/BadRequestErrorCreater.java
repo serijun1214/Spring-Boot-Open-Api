b4aa7ecd5383334d84c513b5a966336485df85f1
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-13 10:57:04 +0900

    タスク一覧の400 Bad Request：不正なフィールドの情報を詰める

--- src/main/java/com/example/SpringBootOpenApi/controller/advice/BadRequestErrorCreater.java
+++ src/main/java/com/example/SpringBootOpenApi/controller/advice/BadRequestErrorCreater.java
@@ -2,22 +2,31 @@ package com.example.SpringBootOpenApi.controller.advice;
 
 import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.InvalidParam;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ElementKind;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class BadRequestErrorCreater {
     public static BadRequestError from(MethodArgumentNotValidException ex) {
-        var invalidParamList = ex.getFieldErrors()
-                .stream()
-                .map(BadRequestErrorCreater::createInvalidParam)
-                .collect(Collectors.toList());
+        var invalidParamList = createInvalidParamList(ex);
         var error = new BadRequestError();
         error.setInvalidParams(invalidParamList);
 
         return error;
+    }
+
+    private static List<InvalidParam> createInvalidParamList(MethodArgumentNotValidException ex) {
+        return ex.getFieldErrors()
+                .stream()
+                .map(BadRequestErrorCreater::createInvalidParam)
+                .collect(Collectors.toList());
     }
 
     private static InvalidParam createInvalidParam(FieldError fieldError) {
@@ -28,6 +37,30 @@ public class BadRequestErrorCreater {
     }
 
     public static BadRequestError from(ConstraintViolationException ex) {
-        return new BadRequestError();
+        var invalidParamList = createParameList(ex.getConstraintViolations()
+                .stream()
+                .map(BadRequestErrorCreater::createInvalidParam));
+
+        var error = new BadRequestError();
+        error.setInvalidParams(invalidParamList);
+
+        return error;
+    }
+
+    private static List<InvalidParam> createParameList(Stream<InvalidParam> ex) {
+        return ex
+                .collect(Collectors.toList());
+    }
+
+    private static InvalidParam createInvalidParam(ConstraintViolation<?> violation) {
+        var parameterOpt = StreamSupport.stream(violation.getPropertyPath().spliterator(), false)
+                .filter(node -> node.getKind().equals(ElementKind.PARAMETER))
+                .findFirst();
+
+        var invalidParam = new InvalidParam();
+        parameterOpt.ifPresent(p -> invalidParam.setName(p.getName()));
+        invalidParam.setReason(violation.getMessage());
+
+        return invalidParam;
     }
 }
